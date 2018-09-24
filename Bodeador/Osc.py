0fb8dc75877872701ffd07845faa805dfdd9e14b
Ezequiel Vijande <evijande@itba.edu.ar>
2018-09-23 22:55:10 -0300

    Estoy empezando la clase del osciloscopio

---
+++ Bodeador/Osc.py
@@ -0,0 +1,12 @@
+class Osc(object):
+    """ Clase que se ocupa de guardar y manejar la configuracion del osciloscopio"""
+    timeout
+    X_Blocks=10
+    Y_Blocks=10
+
+    def __init__(self):
+
+
+      
+
+
