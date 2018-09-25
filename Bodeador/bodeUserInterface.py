00ffe4f30fafd3de970d6852c8bdc5e49f1cb7a3
matias larroque <larroquematias@gmail.com>
2018-09-25 18:14:09 -0300

    se añade archivo para debuggear gui

--- Bodeador/bodeUserInterface.py
+++ Bodeador/bodeUserInterface.py
@@ -21,13 +21,21 @@ X10 = 2
 
 class graphicalInterface:
     def __init__(self):
-        #userData
-        self.instData = instrumentsData()
+        #userData a completar
         self.measData = measurementsData()
         self.pData = preferenceData()
+
+        #variables auxiliares vinculadas a los resources gráficos
+        self.isMinScaleHz = True
+        self.isMinScaleKHz = False
+        self.fMin = 0
+
+        self.isMaxScaleHz = True
+        self.isMaxScaleKHz = False
+        self.fMax = 0
         #graphicalResources
         self.raiz = Tk()
-        self.raiz.geometry('800x680')
+        self.raiz.geometry('960x620')
 
         self.raiz.resizable(width=True,height=True)
         self.raiz.title('Auto Bode Software')
@@ -37,32 +45,6 @@ class graphicalInterface:
         self.placeEntryText()
         self.placeSliders()
         self.placeComboBoxes()
-        
-
-        #variables vinculadas a los graphical resources
-        self.isMinScaleHz = True
-        self.isMinScaleKHz = False
-        self.fMin = 0
-        #setFinalfMin(self)
-
-        self.isMaxScaleHz = True
-        self.isMaxScaleKHz = False
-        self.fMax = 0
-        #setFinalfMax(self)
-
-        self.HighVin = 0
-        self.LowVin = 0
-        #self.setFinalVin(self)
-
-        self.HighOffset = 0
-        self.LowOffset = 0
-        #self.setFinalOffset(self)
-
-        self.isExcelWanted = True
-        self.nombreExcel = "FrequencyResponse" 
-        self.isMagGraphWanted = False
-        self.isFaseGraphWanged = False
-        #setPreferences(self)
 
         self.raiz.mainloop()
         
@@ -70,23 +52,23 @@ class graphicalInterface:
     def placeRadioButtons(self):
         self.scaleStartF = tk.IntVar()
         self.rButton_startF_Hz = tk.Radiobutton(self.raiz, text="Hz", 
-            variable = self.scaleStartF, value = Hz, command = self.refreshFminScale()).grid(row = 2, column = 3)
+            variable = self.scaleStartF, value = Hz, command = self.refreshFminScale).grid(row = 2, column = 3)
     
         self.rButton_startF_KHz = tk.Radiobutton(self.raiz, text="KHz", 
-                            variable = self.scaleStartF, value = KHz, command = self.refreshFminScale()).grid(row = 3, column = 3)
+                            variable = self.scaleStartF, value = KHz, command = self.refreshFminScale).grid(row = 3, column = 3)
 
         self.scaleStopF = tk.IntVar()
         self.rButton_stopF_Hz = tk.Radiobutton(self.raiz, text="Hz", 
-                                variable = self.scaleStopF, value = Hz, command = self.refreshFmaxScale()).grid(row = 4, column = 3)
+                                variable = self.scaleStopF, value = Hz, command = self.refreshFmaxScale).grid(row = 4, column = 3)
         self.rButton_stopF_KHz = tk.Radiobutton(self.raiz, text="KHz", 
-                                variable = self.scaleStopF, value = KHz, command = self.refreshFmaxScale()).grid(row = 5, column = 3)
+                                variable = self.scaleStopF, value = KHz, command = self.refreshFmaxScale).grid(row = 5, column = 3)
 
         self.labelCoupling = tk.Label(self.raiz, text = "Coupling").grid(row = 3, column = 4)
         self.coupling = tk.IntVar()
         self.rButton_couplingAC = tk.Radiobutton(self.raiz, text="couple AC", 
-                                variable = self.TKvarCoupling, value = AC, command = self.refreshCoupling()).grid(row =3, column = 5)
+                                variable = self.coupling, value = AC, command = self.refreshCoupling).grid(row =3, column = 5)
         self.rButton_couplingDC = tk.Radiobutton(self.raiz, text="couple DC", 
-                                variable = self.TKvarCoupling, value = DC, command = self.refreshCoupling()).grid(row =4, column = 5)
+                                variable = self.coupling, value = DC, command = self.refreshCoupling).grid(row =4, column = 5)
         
         self.labelAcq = tk.Label(self.raiz, text = "Acquire").grid(row = 5, column = 4)
         self.acq = tk.IntVar()
@@ -104,33 +86,31 @@ class graphicalInterface:
         self.labelSweepMode = tk.Label(self.raiz, text = "Sweep Mode").grid(row = 1, column = 4)
         self.sweepType = tk.IntVar()
         self.rButton_sweepLog = tk.Radiobutton(self.raiz, text="Logarithmic sweep", 
-                                variable = self.TKvarSweepType, value = LOG, command = self.refreshSweepType).grid(row =1, column = 5)
+                                variable = self.sweepType, value = LOG, command = self.refreshSweepType).grid(row =1, column = 5)
         self.rButton_sweepLinear = tk.Radiobutton(self.raiz, text="Linear sweep", 
-                                variable = self.TKvarSweepType, value = LINEAR, command = self.refreshSweepType).grid(row =2, column = 5)
+                                variable = self.sweepType, value = LINEAR, command = self.refreshSweepType).grid(row =2, column = 5)
 
         self.labelProbe = tk.Label(self.raiz, text = "Probe").grid(row = 3, column = 6)
         self.probe = tk.IntVar()
         self.rButton_probeX1 = tk.Radiobutton(self.raiz, text = "X1", variable = self.probe, 
-                                    value = X1, command = self.refreshProbe()).grid(row = 4, column = 6)
+                                    value = X1, command = self.refreshProbe).grid(row = 4, column = 6)
         self.rButton_probeX10 = tk.Radiobutton(self.raiz, text = "X10", variable = self.probe, 
-                                    value = X10, command = self.refreshProbe()).grid(row = 5, column = 6)
+                                    value = X10, command = self.refreshProbe).grid(row = 5, column = 6)
         
 
     def refreshFminScale(self):
         aux = self.scaleStartF.get()
+        print(aux)
         if aux == Hz:
             self.isMinScaleHz = True
             self.isMinScaleKHz = False
         elif aux == KHz:
             self.isMinScaleHz = False
             self.isMinScaleKHz = True
-
-        self.setFinalfMin()
+        self.setFinalfMin
 
     def setFinalfMin(self):
-
         if self.isMinScaleHz:
-
             self.measData.fMin = self.fMin
 
         elif self.isMinScaleKHz:
@@ -145,7 +125,7 @@ class graphicalInterface:
             self.isMaxScaleHz = False
             self.isMaxScaleKHz = True
             
-        self.setFinalfMax()
+        self.setFinalfMax
 
     def setFinalfMax(self):
         if self.isMaxScaleHz:
@@ -216,7 +196,7 @@ class graphicalInterface:
             self.measData.probeX1 = True
             self.measData.probeX10 = False
 
-        elif aux == X2:
+        elif aux == X10:
             self.measData.probeX1 = False
             self.measData.probeX10 = True
 
@@ -226,6 +206,10 @@ class graphicalInterface:
         self.botonQuitProgram = tk.Button(self.raiz, text = "Quit", command=self.quitProgram_button).grid(row = 14, column = 5)
 
     def goBode_button(self):
+        
+        self.refreshTime2Establish() #se obtiene el valor de un combobox ya que dicho combobox no puede
+                                    #obtener su valor por si solo
+        self.setExcelName()
         self.pData.userWantContinue = True
         self.raiz.destroy()
 
@@ -237,75 +221,82 @@ class graphicalInterface:
         self.labelPreferences = tk.Label(self.raiz, text = "Other Preferences").grid(row = 9, column = 4)
         self.bool_excel = tk.BooleanVar()
         self.excel_checkButton = tk.Checkbutton(self.raiz, text="Create Excel with measures", 
-                                                                    variable=self.bool_excel, command = self.refreshExcelPreference()).grid(row = 10, column = 5)
+                                                                    variable=self.bool_excel, command = self.refreshExcelPreference).grid(row = 10, column = 5)
         self.bool_graphMag = tk.BooleanVar()
         self.graphMag_checkButton = tk.Checkbutton(self.raiz, text="Graph magnitude diagram", 
-                                                                variable=self.bool_graphMag, command = self.refreshGraphMagPreference()).grid(row = 9, column = 5)
+                                                                variable=self.bool_graphMag, command = self.refreshGraphMagPreference).grid(row = 9, column = 5)
         self.bool_graphFase = tk.BooleanVar()
         self.graphFase_checkButton = tk.Checkbutton(self.raiz, text="Graph phase diagram", 
-                                                                        variable=self.bool_graphFase, command = self.refreshGraphFasePreference()).grid(row = 9, column = 6)
+                                                                        variable=self.bool_graphFase, command = self.refreshGraphFasePreference).grid(row = 9, column = 6)
 
-    def refreshExcelPreference(self):
+    def refreshExcelPreference(self, event):
         self.pData.userWantExcel = self.bool_excel.get()
 
-    def refreshGraphMagPreference(self):
+    def refreshGraphMagPreference(self, event):
         self.pData.userWantGraphMag = self.bool_graphMag.get()
 
-    def refreshGraphFasePreference(self):
+    def refreshGraphFasePreference(self, event):
         self.pData.userWantGraphFase = self.bool_graphFase.get()
 
     def placeEntryText(self):
         self.excelName = tk.StringVar()
-        self.nombreExcel_tk = tk.Entry(self.raiz, textvariable = excelName, command = self.setExcelName()).grid(row = 10, column = 6)
+        self.nombreExcel_tk = tk.Entry(self.raiz, textvariable = self.excelName).grid(row = 10, column = 6)
 
     def setExcelName(self):
         self.pData.excelName = self.excelName.get()
 
     def placeSliders(self):
+
         self.label_freqTitle = tk.Label(self.raiz, text = "Frequency").grid(row = 1, column = 1)
         self.label_startF = tk.Label(self.raiz, text = "Start Frequency").grid(row = 2, column = 1)
         self.slide_fmin = tk.IntVar()
         self.startF_slider = tk.Scale(self.raiz, from_=0, to=5000, length = 200, orient=HORIZONTAL, 
-                    variable = self.slide_fmin, command=self.refreshStartF_slider()).grid(row = 2, column = 2)
+                    variable = self.slide_fmin, command=self.refreshStartF_slider).grid(row = 2, column = 2)
     
+        
         self.label_stopF = tk.Label(self.raiz, text = "Stop Frequency").grid(row = 4, column = 1)
         self.slide_fmax = tk.IntVar()
         self.stopF_slider = tk.Scale(self.raiz, from_=0, to=5000, length =200, orient=HORIZONTAL, 
-                    variable = self.slide_fmax, command=self.refreshStopF_slider()).grid(row = 4, column = 2)
+                    variable = self.slide_fmax, command=self.refreshStopF_slider).grid(row = 4, column = 2)
         
         self.label_inputTitle = tk.Label(self.raiz, text = "Input Signal").grid(row = 6, column = 1)
         self.slide_vin = tk.IntVar()
+        self.slide_mvin = tk.IntVar()
         self.label_Vin = tk.Label(self.raiz, text = "High Input(Vpp)").grid(row = 7, column = 1)
         self.Vin_slider = tk.Scale(self.raiz, from_=0.0, to=20.0, length = 100, orient=HORIZONTAL, 
-                        variable = slide_vin, command=self.refreshVin_slider).grid(row = 7, column = 2)
-        self.slide_mvin = tk.IntVar()
+                        variable = self.slide_vin, command=self.refreshVin_slider).grid(row = 7, column = 2)
         self.label_mVin = tk.Label(self.raiz, text = "Low Input Signal (mVpp)").grid(row = 8, column = 1)
         self.mVin_slider = tk.Scale(self.raiz, from_=0.0, to=1000.0, length = 100, orient=HORIZONTAL, 
-                        variable = slide_mvin, command=self.refreshVin_slider).grid(row = 8, column = 2)
+                                variable = self.slide_mvin, command=self.refreshVin_slider).grid(row = 8, column = 2)
+        self.slide_mvoffset = tk.IntVar()
         self.slide_voffset = tk.IntVar()
         self.label_Voffset = tk.Label(self.raiz, text = "High Offset Signal (Vpp)").grid(row = 9, column = 1)
         self.Voffset_slider = tk.Scale(self.raiz, from_=-10, to=10, length = 100, orient=HORIZONTAL, 
-                            variable = slide_voffset, command=self.refreshVoffset_slider).grid(row = 9, column = 2)
-        self.slide_mvoffset = tk.IntVar()
+                            variable = self.slide_voffset, command=self.refreshVoffset_slider).grid(row = 9, column = 2)
+
         self.label_mVoffset = tk.Label(self.raiz, text = "Low Offset Signal (mVpp)").grid(row = 10, column = 1)
         self.mVin_slider = tk.Scale(self.raiz, from_=-1000, to=1000, length = 100, orient=HORIZONTAL, 
-                                variable = slide_mvoffset, command=self.refreshVoffset_slider).grid(row = 10, column = 2)
+                                variable = self.slide_mvoffset, command=self.refreshVoffset_slider).grid(row = 10, column = 2)
         
         self.slide_npoints = tk.IntVar()
         self.label_Npoints = tk.Label(self.raiz, text = "Number of Points to Meas").grid(row = 13, column = 1)
         self.nPoints_slider = tk.Scale(self.raiz, from_=0, to=400, length = 100, orient=HORIZONTAL, 
-                                        variable = slide_npoints, command=self.refreshNpoints_slider).grid(row = 13, column = 2)
+                                        variable = self.slide_npoints, command=self.refreshNpoints_slider).grid(row = 13, column = 2)
 
-    def refreshStartF_slider(self):
+    def refreshStartF_slider(self, event):
+        print("pase por el callback")
         self.fMin = self.slide_fmin.get()
+        self.setFinalfMin()
 
-    def refreshStopF_slider(self):
+    def refreshStopF_slider(self, event):
         self.fMax = self.slide_fmax.get()
+        self.setFinalfMax()
 
-    def refreshVin_slider(self):
+    def refreshVin_slider(self, event):
+        print("se ha pasado por refreshVin_slider")
         self.measData.Vin = self.slide_vin.get() + (self.slide_mvin.get() / 1000)
 
-    def refreshVoffset_slider(self):
+    def refreshVoffset_slider(self, event):
         self.measData.Voffset = self.slide_voffset.get() + (self.slide_mvoffset.get() / 1000)
 
     def refreshNpoints_slider(self):
@@ -315,13 +306,13 @@ class graphicalInterface:
         
     def placeComboBoxes(self):
         self.labelTime2Establish = tk.Label(self.raiz, text = "Time to Establishment (seg)").grid(row = 11, column = 1)
-        self.comboTime2Establish = ttk.Combobox(self.raiz, command = self.refreshTime2Establish())
+        self.comboTime2Establish = ttk.Combobox(self.raiz)
         self.comboTime2Establish["values"] = [0.5, 1, 1.5, 2, 2.5, 3, 4, 5, 10, 15, 20] #tiempo en segundos
         self.comboTime2Establish.current(2)
         self.comboTime2Establish.grid(row = 11, column = 2)
 
     def refreshTime2Establish(self):
-        self.measData.establish = int(self.comboTime2Establish.get())
+        self.measData.establish = float(self.comboTime2Establish.get())
         
     
 
--- Bodeador/pruebaGui.py
+++ Bodeador/pruebaGui.py
@@ -1,6 +1,42 @@
 #programa de prueba de interfaz grafica
 
 from bodeUserInterface import graphicalInterface
+from userData import measurementsData
+from userData import preferenceData
 
 gui = graphicalInterface()
-print(gui.isExcelWanted)
+print("frecuencia inicial: " + str(gui.measData.fMin) + " Hz")
+print("frecuencia final: " + str(gui.measData.fMax) + " Hz")
+print("Vin: " + str(gui.measData.Vin) + " Volts")
+print("Voffset " + str(gui.measData.Voffset) + " Volts")
+if gui.measData.ACcouple:
+    print("coupling: AC")
+if gui.measData.DCcouple:
+    print("coupling: DC")
+if gui.measData.acqAVER:
+    print("acquire : AVERAGE")
+if gui.measData.acqHRES:
+    print("acquire: HIGH RESOLUTION")
+if gui.measData.acqNORM:
+    print("acquire: NORMAL")
+if gui.measData.acqPEAK:
+    print("acquire: PEAK DETECT")
+if gui.measData.isRangeLinear:
+    print("Sweep mode: LINEAR ")
+if gui.measData.isRangeLog:
+    print("Sweep mode: LOGARITHMIC")
+if gui.measData.probeX1:
+    print("Probe: X1")
+if gui.measData.probeX10:
+    print("Probe: X10")
+if gui.pData.userWantExcel:
+    print("User want to create EXCEl FILE named as " + gui.pData.excelName)
+
+if gui.pData.userWantGraphFase:
+    print("User want to graph phase diagram")
+if gui.pData.userWantGraphMag:
+    print("User want to graph magnitude diagram")
+if gui.pData.userWantContinue:
+    print("User want to continue with the program")
+
+
